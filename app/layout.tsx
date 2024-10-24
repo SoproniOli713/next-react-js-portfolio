17be86c812454080eddf797c5aeac857b43e360e
sop.oli <sopronioli713@gmail.com>
2024-10-24 15:16:16 +0800

    fixed cross origin

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,11 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Charm:wght@400;700&display=swap"
           rel="stylesheet"
